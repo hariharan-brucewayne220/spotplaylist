be26894d261f27291bd05bdaaf20b4226396b43a
hariharan-brucewayne220 <31664045+hariharan-brucewayne220@users.noreply.github.com>
2019-06-24 20:47:51 +0530

    wasted

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ class App extends Component{
        </div>:<button className="btn btn-success" onClick={() => {
             window.location = window.location.href.includes('localhost') 
               ? 'http://localhost:8888/login' 
-              : 'https://better-playlists-backend.herokuapp.com/login' }
+              : 'https://spotifylist-backend.herokuapp.com//login' }
           }
           style={{padding: '20px', fontSize: '50px', marginTop: '20px'}}>Sign in with Spotify</button>
     
